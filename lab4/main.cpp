c0ef0ace6d2266f05477d1995e7908a63042e21c
Bogdan Krasilnikov <bogdan.krasilnikov@ispring.com>
2025-03-22 10:07:47 +0300

    lab4 Порефакторил main

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -26,6 +26,51 @@ Args ParseArgs(int argc, char* argv[])
 	};
 }
 
+void SetPlayerDataCallback(Player& player, ChordGenerator& chordGenerator, sf::VertexArray& waveform)
+{
+	player.SetDataCallback([&](void* output, ma_uint32 frameCount) mutable {
+		auto samples = std::span(static_cast<ma_float*>(output), frameCount);
+		size_t i = 0;
+		for (auto& sample : samples)
+		{
+			sample = chordGenerator.GetNextSample();
+			if (i < waveform.getVertexCount())
+			{
+				waveform[i].position = sf::Vector2f(static_cast<float>(i), static_cast<float>(WINDOW_HEIGHT) / 2 + sample * 200);
+			}
+			++i;
+		}
+	});
+}
+
+void RunVisualization(sf::RenderWindow& window, sf::VertexArray const& waveform)
+{
+	while (window.isOpen())
+	{
+		sf::Event event{};
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.draw(waveform);
+		window.display();
+	}
+}
+
+ChordGenerator InitChordGenerator(std::istream& input, ma_uint32 sampleRate)
+{
+	const Parser parser(input);
+	const auto bpm = parser.GetBpm();
+	const auto chords = parser.GetChords();
+
+	return { sampleRate, bpm, chords, 1.f };
+}
+
 int main(int argc, char* argv[])
 {
 	try
@@ -35,43 +80,13 @@ int main(int argc, char* argv[])
 
 		const auto [inputFileName] = ParseArgs(argc, argv);
 		std::ifstream input(inputFileName);
-		const Parser parser(input);
-		const auto bpm = parser.GetBpm();
-		const auto chords = parser.GetChords();
+
 		Player player(ma_format_f32, 1);
-		ChordGenerator chordGenerator(player.GetSampleRate(), bpm, chords, 1.f);
+		auto chordGenerator = InitChordGenerator(input, player.GetSampleRate());
 
-		player.SetDataCallback([&](void* output, ma_uint32 frameCount) mutable {
-			auto samples = std::span(static_cast<ma_float*>(output), frameCount);
-			size_t i = 0;
-			for (auto& sample : samples)
-			{
-				sample = chordGenerator.GetNextSample();
-				if (i < waveform.getVertexCount())
-				{
-					waveform[i].position = sf::Vector2f(static_cast<float>(i), static_cast<float>(WINDOW_HEIGHT) / 2 + sample * 200);
-				}
-				++i;
-			}
-		});
-
+		SetPlayerDataCallback(player, chordGenerator, waveform);
 		player.Start();
-
-		while (window.isOpen())
-		{
-			sf::Event event{};
-			while (window.pollEvent(event))
-			{
-				if (event.type == sf::Event::Closed)
-				{
-					window.close();
-				}
-			}
-
-			window.clear();
-			window.draw(waveform);
-			window.display();
-		}
+		RunVisualization(window, waveform);
 	}
 	catch (std::exception const& e)
 	{
