bc6e556e6eb9365d225617fc8ba0814e5f22aaa7
Bogdan Krasilnikov <bogdan.krasilnikov@ispring.com>
2025-03-22 08:42:16 +0300

    lab4 Добавил визуализацию

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -4,6 +4,10 @@
 #include <iostream>
 #include <span>
 #include <fstream>
+#include <SFML/Graphics.hpp>
+
+constexpr int WINDOW_WIDTH = 1400;
+constexpr int WINDOW_HEIGHT = 600;
 
 struct Args
 {
@@ -26,6 +30,9 @@ int main(int argc, char* argv[])
 {
 	try
 	{
+		sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Sound Wave Visualization");
+		sf::VertexArray waveform(sf::LineStrip, WINDOW_WIDTH);
+
 		const auto [inputFileName] = ParseArgs(argc, argv);
 		std::ifstream input(inputFileName);
 		Parser parser(input);
@@ -34,18 +41,37 @@ int main(int argc, char* argv[])
 		Player player(ma_format_f32, 1);
 		ChordGenerator chordGenerator(player.GetSampleRate(), bpm, chords, 1.f);
 
-		player.SetDataCallback([&chordGenerator](void* output, ma_uint32 frameCount) mutable {
+		player.SetDataCallback([&](void* output, ma_uint32 frameCount) mutable {
 			auto samples = std::span(static_cast<ma_float*>(output), frameCount);
+			size_t i = 0;
 			for (auto& sample : samples)
 			{
 				sample = chordGenerator.GetNextSample();
+				if (i < waveform.getVertexCount())
+				{
+					waveform[i].position = sf::Vector2f(static_cast<float>(i), static_cast<float>(WINDOW_HEIGHT) / 2 + sample * 200);
+				}
+				++i;
 			}
 		});
 
 		player.Start();
 
-		std::string s;
-		std::getline(std::cin, s);
+		while (window.isOpen())
+		{
+			sf::Event event{};
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+				{
+					window.close();
+				}
+			}
+
+			window.clear();
+			window.draw(waveform);
+			window.display();
+		}
 	}
 	catch (std::exception const& e)
 	{
